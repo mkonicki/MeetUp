16f48f0c37d6f28d49b4a1aae66e4813c521493f
Mateusz.Konicki <mateusz.konicki94@gmail.com>
2017-04-23 21:46:00 +0200

    add passive update

--- app/src/test/java/konicki/mateusz/greendaosample/MatchTest.java
+++ app/src/test/java/konicki/mateusz/greendaosample/MatchTest.java
@@ -45,8 +45,38 @@ public class MatchTest extends BaseTest {
 
         assertThat(matchFromDB.getRedGoals()).isEqualTo(redGoals);
         assertThat(matchFromDB.getBlueGoals()).isEqualTo(blueGoals);
-
     }
 
+    @Test
+    public void matchUpdateWhenEntityIsPasive() {
+        DaoSession session = master.newSession();
+        final int blueGoals = 4;
+        final int redGoals = 3;
+
+        Team teamA = new Team();
+        Team teamB = new Team();
+
+        session.insert(teamA);
+        session.insert(teamB);
+
+        Match match = new Match(MatchType.OneVsOne, teamA, teamB);
+
+        assertThat(session.insert(match)).isNotNull();
+        
+        for (int i = 0; i < blueGoals; i++)
+            match.blueTeamGoal();
+
+        for (int i = 0; i < redGoals; i++)
+            match.redTeamGoal();
+
+        session.getMatchDao().update(match);
+
+        session.clear();
+
+        Match matchFromDB = master.newSession().getMatchDao().load(match.getId());
+
+        assertThat(matchFromDB.getRedGoals()).isEqualTo(redGoals);
+        assertThat(matchFromDB.getBlueGoals()).isEqualTo(blueGoals);
+    }
 
 }
